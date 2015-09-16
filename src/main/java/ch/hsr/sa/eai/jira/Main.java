7e87856e189624095bb14befd1dddd1eb127b0b1
Michi Gysel <mgysel@hsr.ch>
2015-09-16 14:20:54 +0200

    changed worklog extractor to work with the hosted BA JIRA

--- src/main/java/ch/hsr/sa/eai/jira/Main.java
+++ src/main/java/ch/hsr/sa/eai/jira/Main.java
@@ -3,8 +3,11 @@ package ch.hsr.sa.eai.jira;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
@@ -22,24 +25,26 @@ import com.atlassian.jira.rest.client.api.domain.Worklog;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 
 public class Main {
+
+	private static final String EPIC_LINK_CUSTOMFIELD = "customfield_10008";
+	private static final String PROJECT_NAME = "BA";
+	
 	private final Logger log = LoggerFactory.getLogger(Main.class);
 
-	public static void main(String[] args) throws URISyntaxException,
-			IOException, JSONException {
+	public static void main(String[] args) throws URISyntaxException, IOException, JSONException, InterruptedException, ExecutionException {
 		String link = args[0];
 		String user = args[1];
 		String pw = args[2];
 		new Main().start(user, pw, link);
 	}
 
-	private void start(String user, String pw, String link)
-			throws URISyntaxException, IOException, JSONException {
+	private void start(String user, String pw, String link) throws URISyntaxException, IOException, JSONException, InterruptedException, ExecutionException {
 		final AsynchronousJiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
 		URI url = new URI(link);
-		JiraRestClient restClient = factory.createWithBasicHttpAuthentication(
-				url, user, pw);
+		JiraRestClient restClient = factory.createWithBasicHttpAuthentication(url, user, pw);
 		try {
 			Set<String> fields = new HashSet<String>();
+			// fields.add("*all");
 			fields.add("worklog");
 			fields.add("summary");
 			fields.add("issuetype");
@@ -49,39 +54,51 @@ public class Main {
 			fields.add("status");
 			fields.add("parent");
 			fields.add("sprint");
-			final SearchResult issues = restClient.getSearchClient()
-					.searchJql("project='HSR'", 999, 0, fields).claim();
+			fields.add(EPIC_LINK_CUSTOMFIELD); // Epic Link
+			final SearchResult issues = restClient.getSearchClient().searchJql("project='"+PROJECT_NAME+"'", 999, 0, fields).claim();
 			log.info("{} issues found.", issues.getTotal());
-			log.info("Key\tSummary\tParent\tParent Summary\tSpent (min)\t Date\tPerson");
-			DateTimeFormatter formatter = DateTimeFormat
-					.forPattern("yyyy-MM-dd");
+			log.info("Key\tSummary\tParent\tParent Summary\tEpic\tSpent (min)\t Date\tPerson");
+			DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
 			for (Issue issue : issues.getIssues()) {
 				IssueField parent = issue.getField("parent");
 				JSONObject parentValue = null;
 				JSONObject parentFields = null;
-				Object parentKey = null;
-				Object parentSummary = null;
+				String parentKey = null;
+				String parentSummary = null;
 				if (parent != null) {
 					parentValue = (JSONObject) parent.getValue();
 					parentFields = (JSONObject) parentValue.get("fields");
-					parentKey = parentValue.get("key");
-					parentSummary = parentFields.get("summary");
+					parentKey = (String) parentValue.get("key");
+					parentSummary = (String) parentFields.get("summary");
+				} else {
+					parentKey = issue.getKey();
+					parentSummary = issue.getSummary();
 				}
 				// fields
 				String key = issue.getKey();
 				String summary = issue.getSummary();
-				//
+				String epic = extractEpic(parentKey, restClient);
 				for (Worklog w : issue.getWorklogs()) {
 					String person = w.getAuthor().getName();
 					int spent = w.getMinutesSpent();
 					DateTime date = w.getCreationDate();
-					log.info("{}\t{}\t{}\t{}\t{}\t{}\t{}", key, summary,
-							parentKey, parentSummary, spent,
-							formatter.print(date), person);
+					log.info("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}", key, summary, parentKey, parentSummary, epic, spent, formatter.print(date), person);
 				}
 			}
 		} finally {
 			restClient.close();
 		}
+	}
+
+	private static final Map<String, String> EPIC_LINK_CACHE = new HashMap<>();
+	
+	private String extractEpic(String key, JiraRestClient restClient) throws InterruptedException, ExecutionException {
+		if(!EPIC_LINK_CACHE.containsKey(key)) {
+			Issue issue = restClient.getIssueClient().getIssue(key).get();
+			IssueField epicLink = issue.getFieldByName("Epic Link");
+			String epic = (String) (epicLink != null && epicLink.getValue() != null ? epicLink.getValue() : "");
+			EPIC_LINK_CACHE.put(key, epic);
+		}
+		return EPIC_LINK_CACHE.get(key);
 	}
 }
